184d0b205e30e4dfd9075c6b02fc87e3d874bb70
Marat Davletgareev <davletgareev.mn@gmail.com>
2021-08-23 13:03:01 +0300

    fourth commit

--- src/js/__tests__/health.tests.js
+++ src/js/__tests__/health.tests.js
@@ -14,5 +14,20 @@ test('Testing result', () => {
   ];
 
   expect(sortByHealth(initialArray)).not.toBe(sortedArray);
+});
+
+test('Testing result2', () => {
+  const initialArray = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedArray = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
   expect(sortByHealth(initialArray)).toEqual(sortedArray);
 });
